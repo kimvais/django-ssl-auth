e562685ae56dc95169d3dc1668197969911eb490
Kimmo Parviainen-Jalanko <kimvais@ssh.com>
2013-06-11 08:48:43 +0300

    Oops, Django 1.4 compatibility was broken

--- django_ssl_auth/base.py
+++ django_ssl_auth/base.py
@@ -32,7 +32,7 @@ try:
     from django.contrib.auth import get_user_model
     User = get_user_model()
 except ImportError:
-    from django.contrib.auth models import User
+    from django.contrib.auth.models import User
 
 logger = logging.getLogger(__name__)
 
