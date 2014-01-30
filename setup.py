8c181ea00b77cc5a61dfe710771aa7cee3a47746
Kimmo Parviainen-Jalanko <kimvais@ssh.com>
2014-01-30 11:41:19 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='django-ssl-auth',
-      version='1.0',
+      version='1.0.1',
       description='Django SSL Client Authentication',
       author='Kimmo Parviainen-Jalanko',
       author_email='kimvais@ssh.com',
