ef6ea7cbd2f5f1e2cdcf6100b11f2ea99eeff28c
Antti Rasinen <antti.rasinen@codento.com>
2016-11-11 15:28:42 +0200

    Fix errors in testapp
    - enable request.is_secure() method behind nginx
    - fix typos in nginx.conf
    - add ssl_verify_depth to nginx.conf
    - fix urls.py

--- testapp/ssltest/urls.py
+++ testapp/ssltest/urls.py
@@ -1,8 +1,4 @@
-from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
